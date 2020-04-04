2c0268269fd3cb7884f0c2476a596bcbe90a896b
asiafarmik <63172562+asiafarmik@users.noreply.github.com>
2020-04-05 08:15:06 +0900

    atcoder/a/abc161
    First entry, solved problem

---
+++ atcoder/A/ABC_Swap.cpp
@@ -0,0 +1,9 @@
+//https://atcoder.jp/contests/abc161/tasks/abc161_a
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a,b,c;
+  cin >> a >>b >>c;
+  cout<<c<<' ' <<a <<' '<<b;
+}
