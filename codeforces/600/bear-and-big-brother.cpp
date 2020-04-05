c4e4c0e4d4290d598ad59b753d7ca9b2a2e99534
asiafarmik <63172562+asiafarmik@users.noreply.github.com>
2020-04-05 10:29:22 +0900

    Create bear-and-big-brother.cpp

---
+++ codeforces/600/bear-and-big-brother.cpp
@@ -0,0 +1,15 @@
+//https://codeforces.com/problemset/problem/791/A
+#include<iostream>
+using namespace std;
+
+int main() {
+  int a,b;
+  cin >> a >> b;
+  int years=0;
+  while(a<=b) {
+  a= a*3;
+  b = b*2;
+  years++;
+  }
+  cout<<years;
+}
