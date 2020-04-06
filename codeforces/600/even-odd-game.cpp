da30fb60e78aa13b4f197e0aa4f0e8b3dc06f906
asiafarmik <63172562+asiafarmik@users.noreply.github.com>
2020-04-06 15:58:07 +0900

    Create even-odd-game.cpp

---
+++ codeforces/600/even-odd-game.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/contest/959/problem/A
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Mahmoud";
+    }
+    else{
+        cout<<"Ehab";
+    }
+}
